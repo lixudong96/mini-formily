f62ec4fef1bfccd49ab0931d7db652415ad8b638
xudong li <accounts_629032965330d45819a6e546@mail.teambition.com>
2022-07-07 15:38:04 +0800

    init: 创建example项目

---
+++ apps/example/vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// import styleImport from 'vite-plugin-style-import'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    // styleImport({
+    //   libs: [
+    //     {
+    //       libraryName: "antd",
+    //       esModule: true,
+    //       resolveStyle: (name) => {
+    //         return `antd/es/${name}/style/index`;
+    //       }
+    //     }
+    //   ]
+    // })
+  ],
+  resolve: {
+    alias: [
+      { find: /^~/, replacement: "" }
+    ]
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  }
+})
